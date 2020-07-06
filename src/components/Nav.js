e25e1d0935b71c46b3d21f80652ef999dda860c9
Jalaj <Jalaj@mail.com>
2020-07-06 14:16:36 +0530

    brought mobile navbar to life

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { Link } from 'gatsby'
 // import { motion } from 'framer-motion'
 
+//todo
+// 1. fix error coming from Posts.js
+// 2. Learn how to persist state b/w pages
+// 3. how to fix navbar in the bottom (inspect bruskowski)
+
 export default function Nav() {
+  const [showMenu, setShowMenu] = useState(false)
+  const links = useRef(null)
+  console.log(showMenu)
+
+  const hamburgerToggle = (e) => {
+    // console.log(e.currentTarget)
+    const burger = e.currentTarget
+    burger.classList.toggle('opened')
+    burger.setAttribute('aria-expanded', burger.classList.contains('opened'))
+    // links.current.classList.toggle('show')
+    setShowMenu((prev) => !prev)
+
+    // if (showMenu) {
+    //   links.current.classList.add('show')
+    // } else {
+    //   links.current.classList.remove('show')
+    // }
+  }
+
   return (
     <nav className="navbar">
       {/* <div className="flex"> */}
-      <div>
+      <div className="navbar__logo">
         <Link to="/" className="brand">
           {/* <span className="emoji">💾</span>  */}
           &lt;J&gt;
         </Link>
       </div>
-      <div className="navbar__links">
+      <div ref={links} className={'navbar__links' + (showMenu ? ' show' : '')}>
         <Link to="/">
           <svg viewBox="0 0 24 24">
             <path
@@ -53,9 +77,9 @@ export default function Nav() {
       </div>
 
       <div className="navbar__social">
-        <Link
+        <a
           className="navbar__social--twitter"
-          to="https://twitter.com/codewithjalaj"
+          href="https://twitter.com/codewithjalaj"
         >
           <svg
             version="1.1"
@@ -66,8 +90,8 @@ export default function Nav() {
           >
             <path d="M32 7.075c-1.175 0.525-2.444 0.875-3.769 1.031 1.356-0.813 2.394-2.1 2.887-3.631-1.269 0.75-2.675 1.3-4.169 1.594-1.2-1.275-2.906-2.069-4.794-2.069-3.625 0-6.563 2.938-6.563 6.563 0 0.512 0.056 1.012 0.169 1.494-5.456-0.275-10.294-2.888-13.531-6.862-0.563 0.969-0.887 2.1-0.887 3.3 0 2.275 1.156 4.287 2.919 5.463-1.075-0.031-2.087-0.331-2.975-0.819 0 0.025 0 0.056 0 0.081 0 3.181 2.263 5.838 5.269 6.437-0.55 0.15-1.131 0.231-1.731 0.231-0.425 0-0.831-0.044-1.237-0.119 0.838 2.606 3.263 4.506 6.131 4.563-2.25 1.762-5.075 2.813-8.156 2.813-0.531 0-1.050-0.031-1.569-0.094 2.913 1.869 6.362 2.95 10.069 2.95 12.075 0 18.681-10.006 18.681-18.681 0-0.287-0.006-0.569-0.019-0.85 1.281-0.919 2.394-2.075 3.275-3.394z"></path>
           </svg>
-        </Link>
-        <Link to="https://github.com/codewithjalaj">
+        </a>
+        <a href="https://github.com/codewithjalaj">
           <svg
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
@@ -77,8 +101,8 @@ export default function Nav() {
           >
             <path d="M10 19c0 1.141-0.594 3-2 3s-2-1.859-2-3 0.594-3 2-3 2 1.859 2 3zM20 19c0 1.141-0.594 3-2 3s-2-1.859-2-3 0.594-3 2-3 2 1.859 2 3zM22.5 19c0-2.391-1.453-4.5-4-4.5-1.031 0-2.016 0.187-3.047 0.328-0.812 0.125-1.625 0.172-2.453 0.172s-1.641-0.047-2.453-0.172c-1.016-0.141-2.016-0.328-3.047-0.328-2.547 0-4 2.109-4 4.5 0 4.781 4.375 5.516 8.188 5.516h2.625c3.813 0 8.188-0.734 8.188-5.516zM26 16.25c0 1.734-0.172 3.578-0.953 5.172-2.063 4.172-7.734 4.578-11.797 4.578-4.125 0-10.141-0.359-12.281-4.578-0.797-1.578-0.969-3.437-0.969-5.172 0-2.281 0.625-4.438 2.125-6.188-0.281-0.859-0.422-1.766-0.422-2.656 0-1.172 0.266-2.344 0.797-3.406 2.469 0 4.047 1.078 5.922 2.547 1.578-0.375 3.203-0.547 4.828-0.547 1.469 0 2.953 0.156 4.375 0.5 1.859-1.453 3.437-2.5 5.875-2.5 0.531 1.062 0.797 2.234 0.797 3.406 0 0.891-0.141 1.781-0.422 2.625 1.5 1.766 2.125 3.938 2.125 6.219z"></path>
           </svg>
-        </Link>
-        <Link to="https://codepen.io/jalajavascript">
+        </a>
+        <a href="https://codepen.io/jalajavascript">
           <svg
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
@@ -88,8 +112,8 @@ export default function Nav() {
           >
             <path d="M29.555 11.501l-14-9.333c-0.336-0.224-0.774-0.224-1.109 0l-14 9.333c-0.278 0.185-0.445 0.498-0.445 0.832v9.333c0 0.334 0.167 0.647 0.445 0.832l14 9.333c0.168 0.112 0.361 0.168 0.555 0.168s0.387-0.056 0.555-0.168l14-9.333c0.278-0.185 0.445-0.498 0.445-0.832v-9.333c0-0.334-0.167-0.647-0.445-0.832zM15 20.465l-5.197-3.465 5.197-3.465 5.197 3.465-5.197 3.465zM16 11.798v-6.93l11.197 7.465-5.197 3.465-6-4zM14 11.798l-6 4-5.197-3.465 11.197-7.465v6.93zM6.197 17l-4.197 2.798v-5.596l4.197 2.798zM8 18.202l6 4v6.93l-11.197-7.465 5.197-3.465zM16 22.202l6-4 5.197 3.465-11.197 7.465v-6.93zM23.803 17l4.197-2.798v5.596l-4.197-2.798z"></path>
           </svg>
-        </Link>
-        <Link to="https://linkedin.com/jalajgupta">
+        </a>
+        <a href="https://linkedin.com/jalajgupta">
           <svg
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
@@ -101,7 +125,26 @@ export default function Nav() {
             <path d="M2 12h6v18h-6v-18z"></path>
             <path d="M8 7c0 1.657-1.343 3-3 3s-3-1.343-3-3c0-1.657 1.343-3 3-3s3 1.343 3 3z"></path>
           </svg>
-        </Link>
+        </a>
+      </div>
+      <div className="navbar__hamburger">
+        <button
+          className="menu"
+          onClick={hamburgerToggle}
+          aria-label="Main Menu"
+        >
+          <svg width="50" height="50" viewBox="0 0 100 100">
+            <path
+              className="line line1"
+              d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+            />
+            <path className="line line2" d="M 20,50 H 80" />
+            <path
+              className="line line3"
+              d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+            />
+          </svg>
+        </button>
       </div>
       {/* </div> */}
     </nav>
