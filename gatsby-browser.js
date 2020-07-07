d856a4f1a36baffdd74990f3bdf605a18be1e9e6
Jalaj <Jalaj@mail.com>
2020-07-07 13:07:13 +0530

    hidden navbar on scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import NavbarWrapper from './src/components/NavbarWrapper'
 
-export const wrapPageElement = ({ element, props }) => {
-  return <NavbarWrapper {...props}>{element}</NavbarWrapper>
-}
+// export const wrapPageElement = ({ element, props }) => {
+//   return <NavbarWrapper {...props}>{element}</NavbarWrapper>
+// }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Helmet from 'react-helmet'
+import { throttle } from '../utils/helpers'
 
 import favicon from '../../content/thumbnails/favicon.png'
 
 import Nav from './Nav'
-import Footer from './Footer'
+// import Footer from './Footer'
 
 import '../style.css'
 import '../new-moon.css'
 
 export default function Layout({ children }) {
+  const [hideNav, setHideNav] = useState(false)
+  let prevScrollPos = 0
+  useEffect(() => {
+    if (typeof window != undefined) {
+      prevScrollPos = window.pageYOffset
+      window.onscroll = throttle(handleIt, 500)
+    }
+  }, [])
+  const handleIt = () => {
+    let currentScrollPos = window.pageYOffset
+    if (prevScrollPos > currentScrollPos) {
+      setHideNav(false)
+    } else {
+      setHideNav(true)
+    }
+    prevScrollPos = currentScrollPos
+  }
+
   return (
     <>
       <Helmet>
         <link rel="shortcut icon" type="image/png" href={favicon} />
       </Helmet>
-      <div className="site-wrapper">
-        <Nav />
+      <Nav hideNav={hideNav} />
+      <div
+        // onWheel={throttle(handleIt, 1000, { trailing: false })}
+        className="site-wrapper"
+      >
         <main className="main-area">{children}</main>
         {/* <Footer /> */}
       </div>
